3d36937bf9f98f57769c386ee76655b82e805452
Amos <igefad12@gmail.com>
2024-08-19 09:50:53 +0100

    TRACKER: add tailwindcss

---
+++ tracker/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const corePlugins = {
+  preflight: false,
+};
+export const content = [
+  "./src/**/*.{html,ts}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
